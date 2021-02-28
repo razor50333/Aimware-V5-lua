0c39ced2663c51fbef1d3dd68c5e677e9d145c35
razor50333 <65458943+razor50333@users.noreply.github.com>
2021-02-28 15:22:27 +0530

    added more indicators

--- Anti-aim circle.lua
+++ Anti-aim circle.lua
@@ -6,6 +6,9 @@ local icp = gui.ColorPicker(group, "hi_iclr", "Indicator Color", 255,75,15,255)
 local shiftind = gui.Checkbox(group, "hi_shift", "Shift On Shot Indicator", 1)
 local dtind = gui.Checkbox(group, "hi_dt", "Double Tap Indicator", 1)
 local attind = gui.Checkbox(group, "hi_at", "At Targets Indicator", 1)
+local aeind = gui.Checkbox(group, "hi_ae", "At Edge Indicator", 1)
+local fklag = gui.Checkbox(group, "hi_fklag", "Fake lag Indicator", 1)
+local fkdck = gui.Checkbox(group, "hi_fkdck", "Fake Duck Indicator", 1)
 
 local font = draw.CreateFont("Calibri", 22, 1000)
 
@@ -69,7 +72,7 @@ local function circle()
             end
 
             draw.Triangle(cur_point[1], cur_point[2], old_point[1], old_point[2], old_point2[1], old_point2[2])
-            draw.Triangle(cur_point2[1], cur_point2[2], old_point2[1], old_point2[2], cur_point[1], cur_point[2])     
+            draw.Triangle(cur_point2[1], cur_point2[2], old_point2[1], old_point2[2], cur_point[1], cur_point[2])    
         
         end
 
@@ -78,7 +81,7 @@ local function circle()
 
         if shiftind:GetValue() then
             offset = offset + 1
-            if gui.GetValue("rbot.antiaim.extra.shiftonshot") then
+            if gui.GetValue("rbot.antiaim.condition.shiftonshot") then
                 draw.Color(ir, ig, ib, ia)
             else
                 draw.Color(bgr, bgg, bgb, bga)
@@ -98,14 +101,44 @@ local function circle()
 
         if attind:GetValue() then
             offset = offset + 1
-            if gui.GetValue("rbot.antiaim.attargets") ~= 0 then
+            if gui.GetValue("rbot.antiaim.advanced.autodir.targets") then
                 draw.Color(ir, ig, ib, ia)
             else
                 draw.Color(bgr, bgg, bgb, bga)
             end
-            draw.TextShadow(x - draw.GetTextSize("AT TARGETS")/2, y + radius + offset * 18, "AT TARGETS")
+            draw.TextShadow(x - draw.GetTextSize("DOUBLE TAP")/2, y + radius + offset * 18, "AT TARGETS")
         end
+        
+        if aeind:GetValue() then
+            offset = offset + 1
+            if gui.GetValue("rbot.antiaim.advanced.autodir.edges") then
+                draw.Color(ir, ig, ib, ia)
+            else
+                draw.Color(bgr, bgg, bgb, bga)
+            end
+            draw.TextShadow(x - draw.GetTextSize("DOUBLE TAP")/2, y + radius + offset * 18, "AT EDGE")
+        end
+        if fklag:GetValue() then
+            offset = offset + 1
+            if gui.GetValue("misc.fakelag.enable") == true then
+                draw.Color(ir, ig, ib, ia)
+            else
+                draw.Color(bgr, bgg, bgb, bga)
+            end
+            draw.TextShadow(x - draw.GetTextSize("DOUBLE TAP")/2, y + radius + offset * 18, "FAKE LAG")
+        end
+          
+        if fkdck:GetValue() then
+            offset = offset + 1
+            local fkdck_key = gui.GetValue("rbot.antiaim.extra.fakecrouchkey")
 
+        if input.IsButtonDown(fkdck_key) then
+            draw.Color(ir, ig, ib, ia)
+            else
+                draw.Color(bgr, bgg, bgb, bga)
+            end
+            draw.TextShadow(x - draw.GetTextSize("DOUBLE TAP")/2, y + radius + offset * 18, "FAKE DUCK")
+        end
     end
 end
 
