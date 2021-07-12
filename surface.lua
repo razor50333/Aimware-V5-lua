d5e06f8a2f4f3f461a3ea21199fd24c3f6e1d007
razor50333 <65458943+razor50333@users.noreply.github.com>
2021-07-12 18:19:04 +0530

    Update surface.lua

--- surface.lua
+++ surface.lua
@@ -4,6 +4,7 @@ local bit_bor = bit.bor
 local ffi_string = ffi.string
 local ffi_cast = ffi.cast
 
+
 local function vmt_entry(instance, index, type)
 	return ffi.cast(type, (ffi.cast("void***", instance)[0])[index])
 end
